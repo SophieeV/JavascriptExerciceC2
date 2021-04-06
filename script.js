15f0eb2af86a8d64ec4c816c422c92627ff06efa
SophieeV <sophievesin@wanadoo.fr>
2021-04-06 11:52:16 +0200

    ajout fichiers

---
+++ script.js
@@ -0,0 +1,10 @@
+function Show (){
+    var show =document.getElementById('button1')
+    var text = document.getElementById('text');
+    text.innerHTML ="<p class='Paragraph'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>";
+}
+function Hide (){
+    var hide =document.getElementById('button2')
+    var text = document.getElementById('text');
+    text.innerHTML ="<p class='Paragraph'></p>";
+}
